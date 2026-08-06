768197b8d25c022e867d60204503412c64f253c9
Alion1233 <dawidinformatykatib@gmail.com>
2026-08-06 19:26:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ app.post('/create-checkout-session', async (req, res) => {
 
     const session = await stripe.checkout.sessions.create({
       // payment_method_types: ['card', 'blik'],
+      managed_payments: {
+        enabled: false,
+      },
       line_items: [
         {
           price_data: {
